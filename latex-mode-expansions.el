4d636ccde7284bd30d89f202a0f9e4d8a15b22f0
Ivan Andrus <darthandrus@gmail.com>
2012-01-29 18:50:14 +0100

    Added expansions for AUCTeX

---
+++ latex-mode-expansions.el
@@ -0,0 +1,55 @@
+;;; latex-mode-expansions.el --- LaTeX-specific expansions for expand-region
+
+;; Copyright (C) 2012 Ivan Andrus
+
+;; Author: Ivan Andrus
+;; Based on js-mode-expansions by: Magnar Sveen <magnars@gmail.com>
+;; Keywords: marking region
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is for AUCTeX, not the builtin latex-mode.
+
+;; Feel free to contribute any other expansions for LaTeX at
+;;
+;;     https://github.com/magnars/expand-region.el
+
+;;; Code:
+
+(defun er/mark-latex-environment ()
+  "Marks the innermost LaTeX environment"
+  (interactive)
+  (er--setup)
+  (LaTeX-mark-environment))
+
+(defun er/mark-latex-section ()
+  "Marks the current section."
+  (interactive)
+  (er--setup)
+  (LaTeX-mark-section))
+
+(defun er/add-latex-mode-expansions ()
+  "Adds expansions for buffers in latex-mode"
+  (set (make-local-variable 'er/try-expand-list) (append
+                                                  er/try-expand-list
+                                                  '(er/mark-latex-environment
+                                                    er/mark-latex-section))))
+
+(add-hook 'LaTeX-mode-hook 'er/add-latex-mode-expansions)
+
+(provide 'latex-mode-expansions)
+
+;; latex-mode-expansions.el ends here
