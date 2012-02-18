511c6b4ba5a5973f7597ac1b2ee01e1e1440f18f
sylvain <sylvain@sylvain-laptop.home>
2012-02-18 15:11:37 +0100

    added enclosing $$ expansion to latex-mode-expansions.el

--- latex-mode-expansions.el
+++ latex-mode-expansions.el
@@ -31,10 +31,19 @@
 
 (defun er/add-latex-mode-expansions ()
   "Adds expansions for buffers in latex-mode"
-  (set (make-local-variable 'er/try-expand-list) (append
-                                                  er/try-expand-list
-                                                  '(LaTeX-mark-environment
-                                                    LaTeX-mark-section))))
+  (set (make-local-variable 'er/try-expand-list)
+       (append
+        er/try-expand-list
+        '(LaTeX-mark-environment)
+        '(LaTeX-mark-enclosing-$))))
+
+(defun LaTeX-mark-enclosing-$ ()
+  "Mark enclosing $"
+  (er--setup)
+  (when (and (texmathp) (equal (car texmathp-why) "$"))
+    (set-mark (cdr texmathp-why))
+    (re-search-forward "\\$" nil t)
+    (exchange-point-and-mark)))
 
 (add-hook 'LaTeX-mode-hook 'er/add-latex-mode-expansions)
 
