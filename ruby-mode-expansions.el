a02e7c54ef963de883b8422776b578165a238c49
Le Wang <le.wang@agworld.com.au>
2012-06-28 10:25:06 +0800

    redo ruby-expansions
    - Many issues with the last hurried commit.

--- ruby-mode-expansions.el
+++ ruby-mode-expansions.el
@@ -29,7 +29,7 @@
 ;;      I think `er/ruby-backward-up' and `er/ruby-forward-up' are very
 ;;      nifty functions in their own right.
 ;;
-;;      I would bind them to C-M-u and C-M-n respectively.
+;;      I would bind them to C-M-u and C-M-d respectively.
 
 ;; Expansions:
 ;;
@@ -42,48 +42,77 @@
 (require 'expand-region-core)
 
 (defvar er/ruby-block-end-re
-  "like ruby-mode's but also for '}'"
-  (concat ruby-block-end-re "\\|}"))
+  (concat ruby-block-end-re "\\|}")
+  "like ruby-mode's but also for '}'")
 
-(defsubst er/ruby-skip-past-block-end ()
+(defun er/ruby-skip-past-block-end ()
   "ensure that point is at bol"
-  (if (looking-at-p er/ruby-skip-past-block-end)
-      (forward-line 1)
-    (forward-line 0)))
+  (when (looking-at er/ruby-block-end-re)
+    (goto-char (match-end 0))))
 
 (defun er/ruby-backward-up ()
   "a la `paredit-backward-up'"
   (interactive)
-  (loop do
-        (let ((orig-point (point))
-              progress-beg
-              progress-end)
-          (ruby-beginning-of-block)
-          (setq progress-beg (point))
-          (ruby-end-of-block)
-          (ruby-skip-past-block-end)
-          (setq progress-end (point))
-          (goto-char progress-beg)
-          (if (> progress-end orig-point)
-              (return)))))
+  (let ((orig-point (point))
+        progress-beg
+        progress-end)
+    ;; cover the case when point is in begining of block
+    (unless (progn (ruby-end-of-block)
+                   (ruby-beginning-of-block)
+                   (< (point) orig-point))
+      (loop do
+            (ruby-beginning-of-block)
+            (setq progress-beg (point))
+            (ruby-end-of-block)
+            (setq progress-end (if (looking-at-p er/ruby-block-end-re)
+                                   (point-at-bol 2)
+                                 (point-at-bol 1)))
+            (goto-char progress-beg)
+            (if (> progress-end orig-point)
+                (return))))))
 
 ;;; This command isn't used here explicitly, but it's symmetrical with
 ;;; `er/ruby-backward-up', and nifty for interactive use.
 (defun er/ruby-forward-up ()
   "a la `paredit-forward-up'"
   (interactive)
-  (ruby-backward-up)
+  (er/ruby-backward-up)
   (ruby-end-of-block)
-  (ruby-skip-past-block-end))
+  (er/ruby-skip-past-block-end))
+
+(defun er/get-ruby-block (&optional pos)
+  "return (beg . end) of current block"
+  (setq pos (or pos (point)))
+  (save-excursion
+    (goto-char pos)
+    (let (beg end)
+      (cons (progn
+              (er/ruby-backward-up)
+              (point))
+            (progn
+              (ruby-end-of-block)
+              (er/ruby-skip-past-block-end)
+              (point))))))
+
+(defun er/mark-ruby-block-up-1 ()
+  (er/ruby-backward-up)
+  (set-mark (point))
+  (ruby-end-of-block)
+  (er/ruby-skip-past-block-end)
+  (exchange-point-and-mark))
 
 (defun er/mark-ruby-block-up ()
-  (interactive)
   "mark the next level up."
-  (ruby-backward-up)
-  (set-mark (point-at-bol 1))
-  (ruby-end-of-block)
-  (ruby-skip-past-block-end)
-  (exchange-point-and-mark))
+  (interactive)
+  (if (use-region-p)
+      (let ((old-end (region-end)))
+        (if (> (cdr (er/get-ruby-block old-end)) old-end)
+            (progn
+              (deactivate-mark)
+              (goto-char old-end)
+              (er/mark-ruby-block-up))
+          (er/mark-ruby-block-up-1)))
+    (er/mark-ruby-block-up-1)))
 
 (defun er/add-ruby-mode-expansions ()
   "Adds Ruby-specific expansions for buffers in ruby-mode"
