dafae103641990f76cc31db3ef0decab167bb86b
Mark Hepburn <Mark.Hepburn@gmail.com>
2012-07-30 09:46:01 +1000

    Adding GPL header.

--- octave-expansions.el
+++ octave-expansions.el
@@ -1,3 +1,31 @@
+;;; octave-expansions.el --- octave-mode expansions for expand-region
+
+;; Copyright (C) 2012 Mark Hepburn
+
+;; Author: Mark Hepburn
+;; Keywords: marking region
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Feel free to contribute any other expansions for Octave at
+;;
+;;     https://github.com/magnars/expand-region.el
+
+;;; Code:
+
 (require 'expand-region-core)
 (require 'octave-mod)
 
