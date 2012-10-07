fa5e320cca8a30c68378eebc23082ab896cb3ef6
thisirs <thisirs@gmail.com>
2012-10-07 23:25:43 +0200

    Enhance display of repeated key
    * expand-region-core.el (er/prepare-for-more-expansions): Use
      `single-key-description' instead of `format-kbd-macro'. This avoids
      descriptions like "\\340".

--- expand-region-core.el
+++ expand-region-core.el
@@ -399,7 +399,7 @@ before calling `er/expand-region' for the first time."
 (defun er/prepare-for-more-expansions ()
   "Let one expand more by just pressing the last key."
   (let* ((repeat-key (event-basic-type last-input-event))
-         (repeat-key-str (format-kbd-macro (vector repeat-key)))
+         (repeat-key-str (single-key-description repeat-key))
          (msg-and-bindings (er/prepare-for-more-expansions-internal repeat-key-str))
          (msg (car msg-and-bindings))
          (bindings (cdr msg-and-bindings)))
